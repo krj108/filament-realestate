0a35567691ebd39d28d131137d0ae99e440dc929
kamaljamous <edwardasawyer1982@gmail.com>
2025-04-02 14:33:15 +0300

    Fix custom fields on users DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable implements HasAvatar
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'custom_fields' => 'array',
     ];
 
     public function getFilamentAvatarUrl(): ?string
